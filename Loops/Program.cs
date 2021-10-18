7fd320802d6bdc2998e753ead3154784e9859217
Harrison Caldwell <h.cald16@gmail.com>
2021-10-18 13:19:58 -0300

    added example of input

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -8,6 +8,15 @@ namespace Loops
         {
             Console.WriteLine("Loops: ");
             Console.ReadKey(true);
+
+            for (int i = 1; i <= 10; i = i + 1)
+            {
+                Console.WriteLine(i);
+            }
+
+            char input;
+            ConsoleKeyInfo readkeyinput = Console.ReadKey();
+            input = readkeyinput.KeyChar;
         }
     }
 }
